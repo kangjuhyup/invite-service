725a0675a035853a4ccf10380691d5fbd1166849
kangjuhyup <fog0510@gmail.com>
2024-10-28 15:32:33 +0900

    fix : creator 전달

--- src/database/repository/user.ts
+++ src/database/repository/user.ts
@@ -1,33 +1,42 @@
-import { UserEntity } from "@database/entity/user";
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { EntityManager, Repository } from "typeorm";
-import { InsertUser, SelectUser } from "./param/user";
+import { UserEntity } from '@database/entity/user';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EntityManager, Repository } from 'typeorm';
+import { InsertUser, SelectUser } from './param/user';
+import { YN } from '@util/yn';
 
 @Injectable()
 export class UserRepository {
-    constructor(
-        @InjectRepository(UserEntity) private readonly user : Repository<UserEntity>
-    ) {}
+  constructor(
+    @InjectRepository(UserEntity) private readonly user: Repository<UserEntity>,
+  ) {}
 
-    async selectUserFromPhone({phone,entityManager}:Omit<SelectUser,'userId'>) {
-        const repo = this._getRepository('user',entityManager);
-        return await repo.findOne({
-            where : {
-                phone
-            }
-        })
-    }
+  async selectUserFromPhone({
+    phone,
+    entityManager,
+  }: Omit<SelectUser, 'userId'>) {
+    const repo = this._getRepository('user', entityManager);
+    return await repo.findOne({
+      where: {
+        phone,
+        useYn: YN.Y,
+      },
+    });
+  }
 
-    async insertUser({user,entityManager}:InsertUser) {
-        const repo = this._getRepository('user',entityManager);
-        return await repo.insert(user);
-    }
+  async insertUser({ user, creator, entityManager }: InsertUser) {
+    const repo = this._getRepository('user', entityManager);
+    return await repo.insert({
+      ...user,
+      creator,
+      updator: creator,
+    });
+  }
 
-    private _getRepository(type: 'user', entityManager?: EntityManager) {
-        if (type === 'user')
-          return entityManager
-            ? entityManager.getRepository(UserEntity)
-            : this.user;
-      }
+  private _getRepository(type: 'user', entityManager?: EntityManager) {
+    if (type === 'user')
+      return entityManager
+        ? entityManager.getRepository(UserEntity)
+        : this.user;
+  }
 }
--- src/domain/user/user.service.ts
+++ src/domain/user/user.service.ts
@@ -1,25 +1,22 @@
+import { UserEntity } from '@database/entity/user';
 import { UserRepository } from '@database/repository/user';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { randomString } from '@util/random';
 @Injectable()
 export class UserService {
- 
-  constructor(
-    private readonly userRepository : UserRepository
-  ) {}
+  constructor(private readonly userRepository: UserRepository) {}
 
-  async getUser(phone: string) {
-    const user = await this.userRepository.selectUserFromPhone({phone});
+  async getUser(phone: string): Promise<UserEntity> {
+    const user = await this.userRepository.selectUserFromPhone({ phone });
     if (!user) throw new UnauthorizedException('존재하지 않는 회원입니다.');
     return user;
   }
 
   async saveUser(phone: string, pwd: string) {
     await this.userRepository.insertUser({
-      user : {nickName : randomString(),
-      phone,
-      password : pwd}
-    })
+      user: { nickName: randomString(), phone, password: pwd },
+      creator: UserService.name,
+    });
 
     return await this.getUser(phone);
   }
