5fea52c635ef7446948345ed9a277d5ce914fce0
kangjuhyup <fog0510@gmail.com>
2024-10-31 21:45:52 +0900

    docs

--- src/domain/auth/dto/sign.ts
+++ src/domain/auth/dto/sign.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
 
 export class SignRequest {
+  @ApiProperty({ description: '휴대폰 번호', example: '01012341234' })
   @IsNotEmpty()
   @IsPhoneNumber()
   phone: string;
 
+  @ApiProperty({ description: '패스워드' })
   @IsNotEmpty()
   @IsString()
   password: string;
