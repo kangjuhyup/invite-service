e7c38d87cc97e5a531d9bb2b45a9b80968459370
kangjuhyup <fog0510@gmail.com>
2024-12-15 22:43:09 +0900

    fix : 경로

--- packages/page/server.js
+++ packages/page/server.js
@@ -1,12 +1,18 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = 5001;
 
-app.use(express.static('./dist'));
+// `__dirname` 구현
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile('./dist/index.html');
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
