962cee4efa75976bf9e06e258634ef0820710c44
kangjuhyup <107104811+kangjuhyup@users.noreply.github.com>
2024-12-16 16:44:38 +0900

    feat : base /page/ 설정

--- packages/page/vite.config.js
+++ packages/page/vite.config.js
@@ -1,9 +1,12 @@
-// vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  cacheDir: '.vite_cache', // `.vite_cache` 폴더에 캐시 저장
+  base : '/page/',
+  css: {
+    modules: {
+      generateScopedName: "[name]__[local]___[hash:base64:5]",
+    },
+  },
 });
