da286bb0f5f490f20d979d2eef389c6b598aa5ba
kangjuhyup <fog0510@gmail.com>
2024-12-18 13:54:43 +0900

    feat : assetPrefix

--- packages/page/next.config.cjs
+++ packages/page/next.config.cjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  basePath: '/page',
   output: 'standalone',
+  reactStricMode: false,
+  assetPrefix: '/page',
 };
 
 module.exports = nextConfig;
