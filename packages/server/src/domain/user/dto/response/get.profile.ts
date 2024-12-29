9dcf4b462fc04e650baf111e6437c63a75336a19
kangjuhyup <fog0510@gmail.com>
2024-12-29 20:10:49 +0900

    refactor : userprofile response 개선

---
+++ packages/server/src/domain/user/dto/response/get.profile.ts
@@ -0,0 +1,24 @@
+import { UserEntity } from "@app/database/entity/user";
+import { IsEmail, IsNotEmpty, IsOptional, IsString, IsUUID } from "class-validator";
+
+export class GetMyProfileResponse {
+    @IsNotEmpty()
+    @IsUUID()
+    id: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsOptional()
+    @IsString()
+    nickName?: string;
+
+    static of(user:UserEntity) {
+        const response = new GetMyProfileResponse();
+        response.id = user.userId;
+        response.email = user.email;
+        response.nickName = user.nickName;
+        return response;
+    }
+}
--- packages/server/src/domain/user/user.controller.ts
+++ packages/server/src/domain/user/user.controller.ts
@@ -1,7 +1,11 @@
 import { UserAccessGuard } from "@app/jwt/guard/user.access.guard";
-import { Controller, Get, Req, UseGuards } from "@nestjs/common";
+import { Controller, Get, Req, UseGuards, UseInterceptors } from "@nestjs/common";
 import { Request } from "express";
 import { UserService } from "./user.service";
+import { HttpResponse } from "../dto/response";
+import { GetMyProfileResponse } from "./dto/response/get.profile";
+import { ApiBearerAuth, ApiOkResponse, ApiOperation } from "@nestjs/swagger";
+import { ResponseValidationInterceptor } from "@app/interceptor/response.validation";
 
 @Controller('user')
 export class UserController {
@@ -10,14 +14,23 @@ export class UserController {
         private readonly userService: UserService
     ){}
 
+    @ApiOperation({ summary: '회원 정보 조회' })
+    @ApiOkResponse({
+        status: 200,
+        description: '성공',
+        type: GetMyProfileResponse,
+    })
+    @ApiBearerAuth()
     @UseGuards(UserAccessGuard)
+    @UseInterceptors(new ResponseValidationInterceptor(GetMyProfileResponse))
     @Get()
     async getMyProfile(
         @Req() request : Request
-    ) {
+    ) : Promise<HttpResponse<GetMyProfileResponse>> {
         const user = request.user as { id: string}
-        return { result : true, data : await this.userService.getUser({
-            userId : user.id
-        })} 
+        return { 
+            result : true, 
+            data : GetMyProfileResponse.of(await this.userService.getUser({userId : user.id}))
+        } 
     }
 }
