8fc172817e1295ec7ba3a39295430924e9b578ae
kangjuhyup <fog0510@gmail.com>
2025-01-21 21:33:18 +0900

    feat : pm2 clustering

---
+++ packages/server/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [{
+    name: 'invite-service',
+    script: 'dist/main.js',
+    instances: 'max',
+    exec_mode: 'cluster',
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
