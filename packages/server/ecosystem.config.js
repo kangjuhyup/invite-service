a0a087394f9a9dbc78d9c313478ea5f8eb75f25d
kangjuhyup <fog0510@gmail.com>
2025-01-22 21:36:01 +0900

    fix : pm2

--- packages/server/ecosystem.config.js
+++ packages/server/ecosystem.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-  apps: [{
-    name: 'invite-service',
-    script: 'dist/main.js',
-    instances: 'max',
-    exec_mode: 'cluster',
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production'
-    }
-  }]
+  apps: [
+    {
+      name: 'invite-service',
+      script: 'dist/main.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+      merge_logs: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+    },
+  ],
 };
