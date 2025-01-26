d90edfd485ec196ac55a73243bae2122f76cf169
kangjuhyup <fog0510@gmail.com>
2025-01-26 12:25:47 +0900

    feat : 배경 꽉차지 않는 이슈 임시 해결

--- packages/page/src/hooks/generate.letter.hook.ts
+++ packages/page/src/hooks/generate.letter.hook.ts
@@ -1,31 +1,37 @@
-import { RefObject } from 'react';
-import { toPng } from 'html-to-image';
-import { FileInfo } from '@/components/image/move/move.resize.image';
-import { TextInfo } from '@/components/text/move/move.text';
+import { RefObject } from "react";
+import { toPng } from "html-to-image";
+import { FileInfo } from "@/components/image/move/move.resize.image";
+import { TextInfo } from "@/components/text/move/move.text";
+import { BACKGROUND_WIDTH, BACKGROUND_HEIGHT } from "@/const";
 
 const useGenerateLetter = (
   backgroundRef: RefObject<HTMLDivElement>,
   files: FileInfo[],
-  texts: TextInfo[],
+  texts: TextInfo[]
 ) => {
   // Letter 이미지 생성
   const generateLetter = async (): Promise<{
     letter: any;
     background: any;
   }> => {
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
     if (!ctx) {
-      throw new Error('Canvas context not available');
+      throw new Error("Canvas context not available");
     }
 
-    if (!backgroundRef.current) throw new Error('BackgroundRef not available');
+    if (!backgroundRef.current) throw new Error("BackgroundRef not available");
 
+    // 실제 크기 계산
+    const width = Number(BACKGROUND_WIDTH.replace("px", ""));
+    const height = Number(BACKGROUND_HEIGHT.replace("px", ""));
+
+    // 캔버스 크기 설정
+    canvas.width = width;
+    canvas.height = height;
+
+    // 배경 이미지 캡처
     const backgroundImage = await toPng(backgroundRef.current);
-    const { offsetHeight: bgHeight, offsetWidth: bgWidth } =
-      backgroundRef.current;
-    canvas.width = bgWidth;
-    canvas.height = bgHeight;
 
     const bg = new Image();
     bg.src = backgroundImage;
@@ -33,21 +39,21 @@ const useGenerateLetter = (
     return new Promise<{ letter: any; background: any }>((resolve, reject) => {
       bg.onload = async () => {
         // 배경 이미지 그리기
-        ctx.drawImage(bg, 0, 0, bgWidth, bgHeight);
+        ctx.drawImage(bg, 0, 0, width + 20, height);
 
         // 이미지 파일 그리기
         const imagePromises = files.map(({ file, size, position }) => {
           return new Promise<void>((resolve) => {
             const img = new Image();
             img.src =
-              typeof file === 'string' ? file : URL.createObjectURL(file);
+              typeof file === "string" ? file : URL.createObjectURL(file);
             img.onload = () => {
               ctx.drawImage(
                 img,
                 position.x,
                 position.y,
                 size.width,
-                size.height,
+                size.height
               );
               resolve();
             };
@@ -58,24 +64,24 @@ const useGenerateLetter = (
         const textPromises = texts.map(
           ({ text, position, size, color, font, bold }) => {
             return new Promise<void>((resolve) => {
-              ctx.font = `${bold ? 'bold' : 'normal'} ${size.width}px ${font}`;
+              ctx.font = `${bold ? "bold" : "normal"} ${size.width}px ${font}`;
               ctx.fillStyle = color;
               ctx.fillText(text, position.x, position.y);
               resolve();
             });
-          },
+          }
         );
 
         await Promise.all([...imagePromises, ...textPromises]);
 
-        const finalImage = canvas.toDataURL('image/png');
+        const finalImage = canvas.toDataURL("image/png");
         resolve({
           letter: finalImage,
           background: backgroundImage,
         });
       };
 
-      bg.onerror = () => reject('초대장 생성 실패.');
+      bg.onerror = () => reject("초대장 생성 실패.");
     });
   };
 
@@ -85,27 +91,26 @@ const useGenerateLetter = (
       const img = new Image();
       img.src = base64Image;
       img.onload = () => {
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
         if (!ctx) {
-          reject('Canvas context not available');
+          reject("Canvas context not available");
           return;
         }
         canvas.width = 100;
         canvas.height = 150;
         ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, 100, 150);
-        const thumbnailBase64 = canvas.toDataURL('image/png');
+        const thumbnailBase64 = canvas.toDataURL("image/png");
         resolve(thumbnailBase64);
       };
-      img.onerror = () => reject('썸네일 생성 실패');
+      img.onerror = () => reject("썸네일 생성 실패");
     });
   };
 
   // DataURL을 File로 변환
   const dataURLToFile = (dataURL: string, fileName: string): File => {
-    const [metadata, base64Data] = dataURL.split(',');
-    const mime = metadata.match(/:(.*?);/)?.[1] || 'image/png';
-
+    const [metadata, base64Data] = dataURL.split(",");
+    const mime = metadata.match(/:(.*?);/)?.[1] || "image/png";
     const binary = atob(base64Data);
     const array = new Uint8Array(binary.length);
     for (let i = 0; i < binary.length; i++) {
