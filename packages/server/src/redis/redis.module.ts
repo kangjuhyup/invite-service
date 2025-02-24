08e03a71737a289bce377ece364cc4f216d52ffc
kangjuhyup <107104811+kangjuhyup@users.noreply.github.com>
2025-02-24 15:20:31 +0900

    Update redis.module.ts

--- packages/server/src/redis/redis.module.ts
+++ packages/server/src/redis/redis.module.ts
@@ -18,8 +18,9 @@ export class RedisClientModule {
     const redisProvider: Provider = {
       provide: REDIS_CLIENT,
       useFactory: async (...args: any[]) => {
+        console.log('RedisClientModule Set RedisProvider');
         const redisOptions = await options.useFactory(...args);
-        console.log(redisOptions);
+        console.log('redisOptions : ' ,redisOptions);
         return new Redis(redisOptions); // Redis 인스턴스 생성
       },
       inject: options.inject || [], // 의존성 주입 설정
